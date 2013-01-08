660c38dc3aec748225d608ec54fee82aa8b0e8b9
Chris Makoumbou <cmakoumbou@gmail.com>
2013-01-08 16:00:17 +0000

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    first_name	"Bat"
-    last_name	"Man"
-    email    "batman@gotham.com"
+    sequence(:first_name)  { |n| "FirstName #{n}" }
+    sequence(:last_name)  { |n| "LastName #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,52 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1',    text: 'All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.first_name + " " + user.last_name)
+        end
+      end
+    end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
@@ -69,5 +115,46 @@ describe "User pages" do
         it { should have_link('Sign out') }
   		end
   	end
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_first_name)  { "New" }
+      let(:new_last_name)   {"Name"}
+      let(:new_email)       { "new@example.com" }
+      before do
+        fill_in "First name",       with: new_first_name
+        fill_in "Last name",        with: new_last_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_first_name) }
+      it { should have_selector('title', text: new_last_name) }
+      it { should have_content('Profile updated') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.first_name.should  == new_first_name }
+      specify { user.reload.last_name.should  == new_last_name }
+      specify { user.reload.email.should == new_email }
+    end
   end
 end
