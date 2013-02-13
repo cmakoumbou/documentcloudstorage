3052fb2f52f01c3a201025f8bb7ac417ab654ae2
Chris Makoumbou <cmakoumbou@gmail.com>
2013-02-13 08:11:28 +0000

    Document upload, show page, upload tests

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,19 @@
 class DocumentsController < ApplicationController
+
   def new
+  	@document = current_user.documents.new
+  end
+
+  def create
+  	@document = current_user.documents.new(params[:document])
+  	if @document.save
+  		redirect_to @document
+  	else
+  		render 'new'
+  	end
+  end
+
+  def show
+  	@document = current_user.documents.find(params[:id])
   end
 end
--- spec/requests/documents_pages_spec.rb
+++ spec/requests/documents_pages_spec.rb
@@ -4,10 +4,32 @@ describe "Document pages" do
 
   subject { page }
 
+  let(:user) { FactoryGirl.create(:user) }
+  before { sign_in user }
+
   describe "new document page" do
     before { visit new_document_path }
 
     it { should have_selector('h1',    text: 'Upload') }
     it { should have_selector('title', text: full_title('Upload')) }
+
+    let(:submit) { "Upload" }
+
+    describe "with no file attached" do
+    	it "should not upload a document" do
+    		expect { click_button submit }.not_to change(Document, :count)
+    	end
+    end
+
+    describe "with file attached" do
+    	before (:each) do
+    		@file_attached = File.join(Rails.root, 'spec', 'support', 'doctor.txt')
+    	end
+    	before { attach_file('Uploaded file', @file_attached) }
+
+    	it "should upload a document" do
+    		expect { click_button submit }.to change(Document, :count).by(1)
+    	end
+    end
   end
 end
